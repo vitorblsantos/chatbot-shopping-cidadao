c15fe5fe6d34504f2d15e832fe803656d458bf06
Vitor Santos <vitorblsantos@gmail.com>
2021-03-28 02:51:43 -0300

    release v 1.0.1

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
-import App from './src/app'
+import App from './src/app.jsx'
 
-render(<App/>, document.getElementById('app'))
+render(<App />, document.getElementById('app'))
