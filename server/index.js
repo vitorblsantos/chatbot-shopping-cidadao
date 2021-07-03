372e6befe91446b1b1e57b4d0c4f599e86d6aa08
vitor barbosa de lima santos <visantos@sbt.com.br>
2021-07-03 18:16:44 -0300

    setup mongo

--- server/index.js
+++ server/index.js
@@ -1,5 +1,17 @@
 import 'dotenv/config'
+import mongoose from 'mongoose'
 
-import App from './app'
+import Server from './server'
 
-App.listen(process.env.PORT || 5000)
+const { MONGO_DB, MONGO_URL, MONGO_PARAMS, API_PORT } = process.env
+
+const urlString = `${MONGO_URL}${MONGO_DB}${MONGO_PARAMS}`
+
+mongoose.connect(urlString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true
+})
+
+mongoose.connection.on('connected', () => Server.listen(API_PORT, () => console.log(`Server running on port: ${API_PORT}`)))
+mongoose.connection.on('error', console.error.bind(console, 'Mongoose connection error:'))
---
+++ server/server.js
@@ -0,0 +1,26 @@
+import { json } from 'body-parser'
+import { resolve } from 'path'
+
+import compression from 'compression'
+import cors from 'cors'
+import express from 'express'
+
+import { Watson, Webhooks } from './routes'
+
+const Server = express()
+
+Server.disable('x-powered-by')
+
+Server.get('/', function (_, res) {
+  res.status(200).sendFile(resolve(__dirname, '../index.html'))
+})
+
+Server.use(cors())
+Server.use(compression())
+Server.use(express.static('./build/'))
+Server.use(json())
+
+Server.use('/watson', Watson)
+Server.use('/webhooks', Webhooks)
+
+export default Server
