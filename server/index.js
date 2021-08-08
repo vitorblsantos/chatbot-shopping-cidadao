7c7aaa1613f480af4ed99c2c0987acb1e9c79102
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2021-08-07 21:11:29 -0300

    feat: adicionando retorno para funcao do sequelize

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import Server from './server'
 const { API_PORT } = process.env
 
 db.authenticate().then(() => {
-  Server.listen(API_PORT, () => console.log(`Server running on port: ${API_PORT}`))
+  return Server.listen(API_PORT || 5000, () => console.log(`Server running on port: ${API_PORT}`))
 }).catch(err => {
   throw new Error(err)
 })
