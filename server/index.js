ec70a70909659e4301b7bd4ffdfa3ae4c26acabd
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2021-08-08 23:16:55 -0300

    feat: ajustando variavel de ambiente das portas

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import Server from './server'
 const { PORT } = process.env
 
 database.authenticate().then(() => {
-  return Server.listen(PORT || 5000, () => console.log(`Server running on port: ${PORT}`))
+  return Server.listen(PORT || 5000, () => console.log(`Server running on port: ${PORT || 5000}`))
 }).catch(err => {
   throw new Error(err)
 })
