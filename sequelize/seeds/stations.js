81e3ef0a8a1d20529b5a7a39f1d22c50da520eb6
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2021-08-24 02:05:28 -0300

    fix: seeds

--- sequelize/seeds/stations.js
+++ sequelize/seeds/stations.js
@@ -1,13 +1,16 @@
+const { v4 } = require('uuid')
+
 const { Stations } = require('../../data/')
-const { DateFNS } = require('../../helpers/')
+const { DateFNS } = require('../../server/helpers/')
 
 const draftStations = Stations.map((station, index) => {
-  station.createdAt = new Date(DateFNS.formated)
-  station.updatedAt = new Date(DateFNS.formated)
+  station._id = v4()
+  station.createdAt = DateFNS.current
+  station.updatedAt = DateFNS.current
   return station
 })
 
 module.exports = {
   down: queryInterface => queryInterface.bulkDelete('stations', null, {}),
-  up: queryInterface => queryInterface.bulkInsert('stations', [...draftStations], {})
+  up: async queryInterface => await queryInterface.bulkInsert('stations', [...draftStations], {})
 }
