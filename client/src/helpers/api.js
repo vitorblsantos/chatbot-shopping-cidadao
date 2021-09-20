60be8ebb9da6a641457209ead96871a35b540ef5
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2021-09-20 02:45:07 -0300

    feat: atualizando deploy

--- client/src/helpers/api.js
+++ client/src/helpers/api.js
@@ -3,7 +3,7 @@
 import 'dotenv/config'
 import { create } from 'axios'
 
-const baseURL = process.env.NODE_ENV === 'production' ? `${window.location.href}` : 'http://localhost:5000/'
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://chatbot-cidadao.herokuapp.com/' : 'http://localhost:5000/'
 
 const Api = create({
   baseURL
