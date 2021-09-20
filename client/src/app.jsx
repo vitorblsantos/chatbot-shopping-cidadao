664f247e7f17b3ce1ed3f75a79f9ff7b2c9ef7aa
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2021-09-20 02:37:45 -0300

    fix: adicionando switch para as rotas

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Active from './pages/active'
 import Cancel from './pages/cancel'
@@ -10,9 +10,11 @@ import './styles/main.scss'
 const App = () => {
   return (
     <Router>
-      <Route path='/' exact component={Home} />
-      <Route path='/ativar/:id' component={Active} />
-      <Route path='/inativar/:id' component={Cancel} />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/ativar/:id' component={Active} />
+        <Route exact path='/inativar/:id' component={Cancel} />
+      </Switch>
     </Router>
   )
 }
